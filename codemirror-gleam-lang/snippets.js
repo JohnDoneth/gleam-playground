91b0d97cf22ad48f42cfe15782ba938a0ec6f8a7
John Doneth <doneth7@gmail.com>
2021-11-29 22:48:59 -0500

    Remove deprecated code

--- codemirror-gleam-lang/snippets.js
+++
@@ -1,9 +0,0 @@
-import { snippetCompletion } from "@codemirror/autocomplete";
-
-export const snippets = [
-  snippetCompletion("fn #{}() ->", {
-    label: "function",
-    detail: "definition",
-    type: "keyword",
-  }),
-];
