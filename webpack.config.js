11bc9707b10b6e58e99d614783297e9246def8ee
inoas <mail@inoas.com>
2021-12-23 19:06:04 +0000

    Fix ios mac safari (#13)
    * fix ios safari, fix mac safari
    
    * readd cssnano
    
    * notabs
    
    * add format script, format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
         test: /\.(png|ttf)$/,
