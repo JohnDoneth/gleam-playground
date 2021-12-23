145b3d32c7586b42c9b519186ebafff0981a9f78
inoas <mail@inoas.com>
2021-12-23 14:24:41 +0100

    fix ios safari, fix mac safari

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,11 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader"
+				],
       },
       {
         test: /\.(png|ttf)$/,
